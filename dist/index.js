e5309f74855d5bf2855148afd0f5a81d19ea2453
FreddyAL1609 <fred_16_09@hotmail.com>
2023-01-18 16:35:48 -0500

    Creando el proyecto instalando dependencia y configurando archivos

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+app.use(express_1.default.json());
+app.get('/home', (req, res) => {
+    res.send('-> Hola');
+});
+app.listen(port, () => {
+    console.log(`El servidor se ejecuta en http://localhost:${port}/home`);
+});
---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+app.use(express.json());
+
+app.get('/home', (req: Request, res: Response) => {
+  res.send('-> Hola');
+});
+
+app.listen(port, () => {
+  console.log(`El servidor se ejecuta en http://localhost:${port}/home`);
+});
